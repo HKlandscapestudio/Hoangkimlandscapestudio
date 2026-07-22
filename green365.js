dcda499febf765c3d276e4aa3329ef6e27619c4c
HKlandscapestudio <ytd2@fpt.edu.vn>
2026-07-22 22:40:42 +0700

    Add initial entries for Green365 project

--- green365.js
+++ green365.js
@@ -1,1 +1,21 @@
+const green365 = [
 
+{
+    day: 1,
+    title: "Bắt đầu Green365",
+    image: "assets/green365/day-001.jpg"
+},
+
+{
+    day: 8,
+    title: "Plant Database",
+    image: "assets/green365/day-008.jpg"
+},
+
+{
+    day: 9,
+    title: "Website Hoàng Kim Landscape Studio",
+    image: "assets/green365/day-009.jpg"
+}
+
+];
